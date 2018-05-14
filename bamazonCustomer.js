877a1792e0d26698e4ad5b89cd9ea663b120d553
Jessica La Torre <jess.latorre@gmail.com>
2018-05-14 17:40:40 -0500

    console logged initial connection results and abbreviated customer list

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,8 +10,17 @@
 // This means updating the SQL database to reflect the remaining quantity.
 // Once the update goes through, show the customer the total cost of their purchase.
 
+//1 - est connection to database
+//2 - now let's query data from database (using JS). NOTE: Console.log to display list of options for customer. This also ensure you're able to return results from database, but after this you'll need to clean up results, so you're able to disply a more simple list to customers
+
 var inquirer = require('inquirer');
 var mysql = require('mysql');
+var colors = require('colors');
+
+//global variable for products to intialize variable
+var products = null;
+
+//var num = 0 will initialize with 0 if you intialize with var num; it will log in console as undefined, and if you're console logging a lot you don't necessarily want to see undefined for a var you initialized if you're trouble shooting other errors
 
 var connection = mysql.createConnection({
   host: 'localhost',
@@ -29,5 +38,21 @@ connection.connect(function(err) {
  
   console.log('HUZZAH! Connected as id ' + connection.threadId);
 });
+
+//snytax from mysql npm
+//connectoin query with cb
+//parameter can be called whatever you want, but the order is imporant. Error always first, then results, then fields.
+connection.query('SELECT * FROM products', function (err, results, fields) {
+    if(err) throw error;
+    // console.log(results); //first console log - this is logged as an array of results
+    //logic to simplify what needs to be displayed to customers
+    //use for loop to do this; can also use for each.
+    //before we do, assign results to products var since it was initialized as null. Assigning it globally as null allows us to use it any function we want to, but we must assign it the correct parameter we're trying to return in each function (as it makes sense);
+    products = results;
+    for (var i = 0; i <products.length; i++) {
+        //second console log - write abbreviated list for customer
+        console.log (products[i].item_id + ' ' + products[i].product_name + ' ' + products[i].price);
+    }
+  });
 
 
